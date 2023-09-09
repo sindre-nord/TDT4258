1ae80ea135707d28364e799e38dd8a274ca084a4
Sindre Nordtveit <sindex82@gmail.com>
2023-09-09 12:44:45 +0200

    Removed comments

--- Examples/write_to_jtag.s
+++ Examples/write_to_jtag.s
@@ -14,10 +14,6 @@ CONT:
 
 PUT_JTAG:
     LDR R1, =0xFF201000 // JTAG UART base address
-    //LDR R2, [R1, #4] // read the JTAG UART control register
-    //LDR R3, =0xFFFF
-    // ANDS R2, R2, R3 // check for write space
-    // BEQ END_PUT // if no space, ignore the character
     STR R0, [R1] // send the character
 END_PUT:
     BX LR
